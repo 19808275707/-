e00ae21ebd8385f96a5b0ec6675b6a7839fecc8d
晚夜微雨问海棠 <2508748367@qq.com>
2022-04-06 14:31:37 +0800

    wy 修改css

--- js/network.js
+++ js/network.js
@@ -1,6 +1,5 @@
 layui.use(['laypage', 'layer'], function(){
     var laypage = layui.laypage
-    ,layer = layui.layer;
         //完整功能
     laypage.render({
       elem: 'fenye',
@@ -11,3 +10,21 @@ layui.use(['laypage', 'layer'], function(){
       }
     });
 })
+
+// 像后端发起ajax请求
+function PyTable(){
+  let urlb = '/cameras/';
+  $.ajax({
+    url: host+urlb,
+    method:"get",
+    success:function(data){
+      console.log(data);
+    }
+
+  })
+
+  
+
+}
+PyTable();
+
